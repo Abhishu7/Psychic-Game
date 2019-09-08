d8d900a3c4032a902852d49f0ec3da4759a461d7
Abhishu Mukherjee <abhishu.m@utexas.edu>
2019-09-08 14:24:14 -0500

    Editting the 'if' statements in the javascript

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,18 +1,37 @@
 var computerChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r",
 "s", "t", "u", "v", "w", "x", "y", "z"];
-var wins = document.getElementById("wins");
-var losses = document.getElementById("losses");
-var guessesLeft = document.getElementById("guessesLeft");
-var guesses = document.getElementById("guesses");
+var wins = 0;
+var losses = 0;
+var guessesLeft = 9;
+var lettersGuessed = "";
+var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
 
+function reset() {
+  computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+  guessesLeft = 9;
+  lettersGuessed = "";
+}
 
 document.onkeyup = function(event) {
     var userGuess = event.key;
-  
-    var computerGuess =
-      computerChoices[Math.floor(Math.random() * computerChoices.length)];
-    
-    if (userGuess === computerGuess){
-        
+  if (computerChoices.includes(userGuess)) {
+    console.log("User Guess: ", userGuess);
+    console.log("Computer Guess: ", computerGuess);
+  if (userGuess === computerGuess) {
+    wins++;
+    reset();
+  } else {
+    guessesLeft--;
+    lettersGuessed= lettersGuessed + ", " + userGuess;
+    if (guessesLeft === 0) {
+      losses++;
+      reset();
     }
+  }
+
+  document.getElementById("wins").innerHTML = wins;
+  document.getElementById("losses").innerHTML = losses;
+  document.getElementById("guesses-left").innerHTML = guessesLeft;
+  document.getElementById("guesses-so-far").innerHTML = lettersGuessed;
+  }
 }
